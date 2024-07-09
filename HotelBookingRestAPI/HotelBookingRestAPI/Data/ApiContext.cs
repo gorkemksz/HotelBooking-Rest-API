ee03df70606d4b4bbbf522476787179844491960
gorkemksz <gorkemoksuz20@gmail.com>
2024-07-10 01:22:51 +0300

    Rest API

---
+++ HotelBookingRestAPI/HotelBookingRestAPI/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingRestAPI.Models;
+using Microsoft.Extensions.Options;
+
+namespace HotelBookingRestAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings{ get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options)
+        :base(options)
+        {
+
+        }
+    }
+}
